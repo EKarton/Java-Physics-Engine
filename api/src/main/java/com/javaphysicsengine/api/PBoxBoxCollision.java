bb5e9512c456ada4e22ec9153e184c3e5161beab
Emilio K <emiliokartono@gmail.com>
2019-04-24 23:31:40 -0400

    Added javadocs to the PBoxBoxCollision class

--- api/src/main/java/com/javaphysicsengine/api/PBoxBoxCollision.java
+++ api/src/main/java/com/javaphysicsengine/api/PBoxBoxCollision.java
@@ -1,20 +1,19 @@
 /*
-  Purpose: To detect whether two bodies collide with each other (why not put it in the PPolygon class? To make it more modular)
-  Original Creation Date: January 1 2016
-  @author Emilio Kartono
-  @version January 15 2016
-*/
-
+ * Purpose: To detect whether two bodies collide with each other
+ * Original Creation Date: January 1 2016
+ * @author Emilio Kartono
+ * @version January 15 2016
+ */
 package com.javaphysicsengine.api;
 
 class PBoxBoxCollision {
-    /*
-      Post-condition: Returns true if two bounding boxes collide; else false
-      Pre-condition: "box1" and "box2" must not be null
-      @param box1 The first bounding box
-      @param box2 The second bounding box
-      @return Returns true if colliding; else false
-    */
+
+    /**
+     * Determines if two bounding boxes collide
+     * @param box1 The first bounding box
+     * @param box2 The second bounding box
+     * @return {@code true} when the two bounding boxes collide; else {@code false}
+     */
     public static boolean doBodiesCollide(PBoundingBox box1, PBoundingBox box2) {
         if (box1.getMaxX() < box2.getMinX()) // If box 1 is left of box 2
             return false;
@@ -27,6 +26,5 @@ class PBoxBoxCollision {
 
         // If box 1 is above box 2
         return !(box1.getMinY() >= box2.getMaxY());
-
     }
 }
