72517e37bc31ac4332d58ebe38f09e4c006910b1
EKarton <emiliokartono@gmail.com>
2019-04-30 12:24:44 -0400

    Added more tests to PSpring and PString

--- api/src/test/java/com/javaphysicsengine/api/body/PSpringTest.java
+++ api/src/test/java/com/javaphysicsengine/api/body/PSpringTest.java
@@ -4,7 +4,13 @@ import com.javaphysicsengine.utils.Vector;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.*;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class PSpringTest {
 
@@ -38,5 +44,18 @@ public class PSpringTest {
 
         assertEquals(Vector.of(45.0, 0.0), circle1.getNetForce());
         assertEquals(Vector.of(-45.0, 0.0), circle2.getNetForce());
+    }
+
+    @Test
+    public void drawConstraints_should_draw_line_between_the_center_points_of_two_objects_in_green() {
+        Graphics graphics = mock(Graphics.class);
+        spring.drawConstraints(graphics, 600);
+
+        verify(graphics, times(1)).setColor(eq(Color.GREEN));
+        verify(graphics, times(1)).drawLine(
+                (int) circle1.getCenterPt().getX(),
+                (int) (600 - circle1.getCenterPt().getY()),
+                (int) circle2.getCenterPt().getX(),
+                (int) (600 - circle2.getCenterPt().getY()));
     }
 }
--- api/src/test/java/com/javaphysicsengine/api/body/PStringTest.java
+++ api/src/test/java/com/javaphysicsengine/api/body/PStringTest.java
@@ -4,11 +4,17 @@ import com.javaphysicsengine.utils.Vector;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.*;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class PStringTest {
 
-    private PString spring;
+    private PString string;
     private PCircle circle2;
     private PCircle circle1;
 
@@ -22,12 +28,12 @@ public class PStringTest {
         circle2.setRadius(10);
         circle2.setCenterPt(Vector.of(100, 10));
 
-        spring = new PString(circle1, circle2);
+        string = new PString(circle1, circle2);
     }
 
     @Test
     public void addTensionForce_should_not_move_circles() {
-        spring.addTensionForce();
+        string.addTensionForce();
 
         assertEquals(Vector.of(0, 0), circle1.getNetForce());
         assertEquals(Vector.of(0, 0), circle2.getNetForce());
@@ -40,12 +46,25 @@ public class PStringTest {
     public void addTensionForce_should_move_right_circle_given_left_circle_has_moved() {
         circle1.move(Vector.of(0, 10));
         circle1.setNetForce(Vector.of(0, 10));
-        spring.addTensionForce();
+        string.addTensionForce();
 
         assertEquals(Vector.of(0, 0), circle1.getNetForce());
         assertEquals(Vector.of(0, 0), circle2.getNetForce());
 
         assertEquals(Vector.of(-5.0, 10.0), circle1.getCenterPt());
         assertEquals(Vector.of(105.0, 10.0), circle2.getCenterPt());
+    }
+
+    @Test
+    public void drawConstraints_should_draw_line_between_the_center_points_of_two_objects_in_white() {
+        Graphics graphics = mock(Graphics.class);
+        string.drawConstraints(graphics, 600);
+
+        verify(graphics, times(1)).setColor(eq(Color.WHITE));
+        verify(graphics, times(1)).drawLine(
+                (int) circle1.getCenterPt().getX(),
+                (int) (600 - circle1.getCenterPt().getY()),
+                (int) circle2.getCenterPt().getX(),
+                (int) (600 - circle2.getCenterPt().getY()));
     }
 }
